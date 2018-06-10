f54e2553c0bff992005711cb3e41c9dd1de7547f
Swapnil-N <swapnil.napuri@gmail.com>
2018-06-10 19:45:19 -0400

    Initial commit

---
+++ core/src/com/mygdx/game/Ballon.java
@@ -0,0 +1,48 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+
+public class Ballon extends Actor{
+
+    Sprite sprite = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
+
+    public Ballon(){
+
+        setX((float)(Math.random()*Gdx.graphics.getWidth()-150)+100);
+        setY((float)(Math.random()*300)+5);
+
+        setBounds(getX(), getY(), sprite.getWidth(), sprite.getHeight());
+        setTouchable(Touchable.enabled);
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        sprite.draw(batch);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        MoveByAction moveByAction = new MoveByAction();
+        moveByAction.setAmountY(100f);
+        moveByAction.setDuration(10f);
+        addAction(moveByAction);
+
+        if (getY() >= 1500)
+            addAction(Actions.removeActor());
+
+        sprite.setPosition(getX(),getY());
+        sprite.setRotation(getRotation());
+        sprite.setScale(getScaleX(),getScaleY());
+    }
+}
---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,127 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+
+import sun.rmi.runtime.Log;
+
+
+public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener{
+
+	Stage stage;
+
+	int score;
+
+	@Override
+	public void create () {
+
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		Ballon ballon1 = new Ballon();
+
+		stage.addActor(ballon1);
+
+		GestureDetector gestureDetector = new GestureDetector(this);
+		Gdx.input.setInputProcessor(gestureDetector);
+
+	}
+
+	@Override
+	public void render () {
+
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (stage.getActors().size < 5){
+			Ballon tempBall = new Ballon();
+			stage.addActor(tempBall);
+		}
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+
+	}
+
+	@Override
+	public void dispose () {
+		stage.dispose();
+	}
+
+
+	@Override
+	public boolean touchDown(float screenX, float screenY, int pointer, int button) {
+		Vector2 coord = stage.screenToStageCoordinates(new Vector2(screenX,screenY));
+		Actor hitActor = stage.hit(coord.x,coord.y,false);
+
+		if (hitActor != null) {
+			Gdx.app.log("asdf","HIT");
+			hitActor.addAction(Actions.removeActor());
+			score++;
+		}else{
+			Gdx.app.log("asdf","NONO");
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean tap(float screenX, float screenY, int count, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		return false;
+	}
+
+	@Override
+	public void pinchStop() {
+
+	}
+}
